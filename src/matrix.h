08d6fb1d4fd2ff30a6ac00a4d1c4b5544f491ffc
Jack Garrard <speyejack@gmail.com>
2016-11-16 19:33:29 -0700

    Fixed vectors

--- src/matrix.h
+++ src/matrix.h
@@ -2,41 +2,44 @@
 #define MATRIX_HEADER_INCLUDED
 #include <vector>
 #include <stdio.h>
+#include <assert.h>
 
 class Matrix {
  private:
-	//	explicit Matrix(Matrix*);
 
 	std::vector<double> matrix;
 	
-	int size;
 	int height;
 	int width;
+	
 	inline void set_direct_value(int i, double value) {matrix[i] = value;}
+	
  public:
-	explicit Matrix(int height, int width);
-
+	
+	Matrix();
+	Matrix(int height, int width);
+	Matrix(const Matrix&);
 	inline int get_width() {return width;}
 	inline int get_height() {return height;}
-	inline int get_size() {return size;}
+	inline int get_size() {return matrix.size();}
 	inline double get_direct_value(int i) {return matrix[i];}
-	inline void set_value(int x, int y, double value) {set_direct_value(width * y + x, value);} 
-	inline double get_value(int x, int y) {return get_direct_value(width * y + x);}
-	inline void operator+=(Matrix &o);
-	inline void operator-=(Matrix &o);
-	inline Matrix operator+(Matrix &o);
-	inline Matrix operator-(Matrix &o);
-	//inline Matrix operator=(Matrix &o);
+	inline void set_value(int x, int y, double value) {assert (y < height && x < width); set_direct_value(width * y + x, value);} 
+	inline double get_value(int x, int y) {assert (y < height && x < width); return get_direct_value(width * y + x);}
+	void operator+=(const Matrix &o);
+	void operator-=(const Matrix &o);
+	Matrix operator+(const Matrix &o);
+	Matrix operator-(const Matrix &o);
+    Matrix& operator=(const Matrix &o);
 	Matrix dot(Matrix &o);
 };
 
 static inline void printMatrix(Matrix &o){
-	printf("W: %d H: %d\n", o.get_width(), o.get_height());
 	printf("{{");
 	for (int i = 0; i < o.get_size(); i++){
 		printf("%f", o.get_direct_value(i));
 		if (i == (o.get_size() - 1))
 			printf("}");
+	   
 		else if ((i + 1) % o.get_width() == 0)
 			printf("},{");
 		else
@@ -44,6 +47,6 @@ static inline void printMatrix(Matrix &o){
 			
 	}
 	printf("}\n");
-		
+	// printf("W: %d H: %d S: %d\n", o.get_width(), o.get_height(), o.get_size());
 }
 #endif
