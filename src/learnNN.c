cc4adc9d5c969c47984d913ba5eccb056c644e9f
Jack Garrard <speyejack@gmail.com>
2016-11-26 17:34:49 -0700

    AND gate works!

--- src/learnNN.c
+++ src/learnNN.c
@@ -1,21 +1,36 @@
 #include <stdio.h>
 #include "matrix.h"
 #include <vector>
-#include "textTrainer.h"
+#include "trainer.h"
 #include "network.h"
-#include <iostream>
+#include <cstdlib>
+#include <ctime>
 
 int main(){
-	std::vector<int> layers = {128, 128, 128};
-	Network net(layers, {1432,114361, 19291992, 192032});
-	TextTrainer t(&net, 100, 0.1, "input/dream.txt");
-	for( int i = 0; i < 1000; i ++){
-		t.train();
-	}
-	char c = -1;
-	for (int i = 0; i < 20; i++){
-		c = t.sample(c);
-		std::cout << c;
-	}
+	std::vector<int> layers = {2, 1};
+	Network n(layers, {1,1});
+	Trainer t(&n, 100, 0.1);
+	srand((unsigned) time(NULL));
+	for(int i = 0; i < 100000; i++){
+		int count = rand() % 4;
 		
+		std::vector<double> input = {(double) (count & 1), (double) ((count >> 1) & 1)};
+		std::vector<double> true_output = {(double) (count == 3)};
+		t.train(input, true_output);
+	}
+	
+	for(int i = 0; i < 4; i++){
+		int count = i % 4;
+		
+		std::vector<double> input = {(double) (count & 1), (double) ((count >> 1) & 1)};
+		std::vector<double> output = n.forward_prop(input);
+		std::vector<double> true_output = {(double) (count == 3)};
+		printf("Vect: \n");
+		Vector input_vec(input);
+		Vector output_vec(output);
+		Vector true_output_vec(true_output);
+		printMatrix(input_vec);
+		printMatrix(output_vec);
+		printMatrix(true_output_vec);
+	}
 }
