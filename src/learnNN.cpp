2dcb3eb3dc39fef0d72d6a357d0541b6b8a3ef59
Jack Garrard <speyejack@gmail.com>
2017-02-10 21:12:03 -0700

    Debugging stuff for nan appearance

--- src/learnNN.cpp
+++ src/learnNN.cpp
@@ -1,5 +1,3 @@
-#include <stdio.h>
-#include "matrix.h"
 #include <vector>
 #include "gateTrainer.h"
 #include "network.h"
@@ -12,21 +10,31 @@ int main(){
 	std::vector<int> layers = {2, 1};
 	unsigned long seed = time(0);
 	std::cout << "Seed: " << seed << std::endl;
-	seed = 1485472172;
+	seed = 1486755178;
 	Network net(layers, {seed});
 	
-	GateTrainer t(&net, 1000, 0.01,
+	GateTrainer t(&net, 1000, 0.1,
 				  [](bool input1, bool input2) -> bool {
-					  return input1 && input2;
+					  return input1 || input2;
 				  });
-	for( int i = 0; i < 100000; i ++){
+	for( int i = 0; i < 1000; i++){
 		t.train();
-		printf("Sampling...\n");
+		printf("Sampling... Iter:%d\n", i);
+		// 10th run breaks
+		bool nantest = false;
+		bool zerotest = true;
 		for (int j = 0; j < 4; j++){
-			// 10th run breaks
 			double sample = t.sample(j/2, j%2);
+			nantest = nantest || std::isnan(sample);
+			zerotest = zerotest && !sample; 
 			printf("%d && %d = %d : %.3f\n", j/2 , j%2, sample > 0.5, sample);
 		}
+		
+		if (nantest || zerotest){
+			printf("Found nan or all zeros in sampling, exiting.\n");
+			exit(1);
+		}
+			
 		net.reset();
 		/*
 		  Taken out until it can be properly reimplemented
