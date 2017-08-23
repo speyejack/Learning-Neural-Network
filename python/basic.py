b22e6a1d1e1eb862598b6062a4f14930222dcf52
Jack <speyejack@gmail.com>
2017-08-23 00:29:01 -0700

    Changed basic NN to be in class form

--- python/basic.py
+++ python/basic.py
@@ -1,69 +1,107 @@
 import numpy as np
 import random
+import pdb
 from time import sleep
 
-input_layer = 2
-hidden_layer = 10
-output_layer = 1
-hidden_weights = np.random.random((hidden_layer, input_layer))
-output_weights = np.random.random((output_layer, hidden_layer))
-a_k = None
-b_h = None
-b_i = None
+class Layer:
+    def __init__(self, input_size, output_size):
+        self.weights = np.random.random((output_size, input_size))
+        self.prev_inputs = []
 
+    def forward_prop(self, input_v):
+        return input_v
 
-def sigmoid(input_v):
-    output_v = 1.0 / (1.0 + np.exp(-input_v))
-    return output_v
+    def backwards_prop(self, output_errors, learning_rate):
+        adjustment_matrix = np.zeros_like(self.weights)
+        return self._backwards_prop(output_errors, learning_rate, adjustment_matrix)
 
+    def _backwards_prop(self, output_errors, learning_rate, adjustment_matrix):
+        if not self.prev_inputs:
+            self.weights -= adjustment_matrix * learning_rate
+            return []
+        error_k = output_errors.pop()
+        input_t = self.prev_inputs.pop()
+        error_i = self.weights.T.dot(error_k) * self.deriv_sigmoid(input_t)
+        adjustment_matrix += error_k.dot(input_t.T)
+        return self._backwards_prop(output_errors, learning_rate, adjustment_matrix) + [error_i]
 
-def deriv_sigmoid(input_v):
-    output_v = input_v * (1 - input_v)
-    return output_v
+    def reset(self):
+        self.prev_inputs = []
 
+    @staticmethod
+    def sigmoid(input_v):
+        output_v = 1.0 / (1.0 + np.exp(-input_v))
+        return output_v
 
-def forward_prop(input_v):
-    global hidden_weights
-    global output_weights
-    global a_k
-    global b_h
-    global b_i
-    b_i = input_v
-    a_h = hidden_weights.dot(b_i)
-    b_h = sigmoid(a_h)
-    a_k = output_weights.dot(b_h)
-    return a_k
+    @staticmethod
+    def deriv_sigmoid(input_v):
+        output_v = input_v * (1 - input_v)
+        return output_v
 
+class OutputLayer(Layer):
+    def forward_prop(self, input_v):
+        self.prev_inputs.append(input_v)
+        a = self.weights.dot(input_v)
+        return a
 
-def backwards_prop(error_k, learning_rate=0.001):
-    global hidden_weights
-    global output_weights
-    global a_k
-    global b_h
-    global b_i
-    error_h = output_weights.T.dot(error_k) * deriv_sigmoid(b_h)
-    hidden_weights -= error_h.dot(b_i.T) * learning_rate
-    output_weights -= error_k.dot(b_h.T) * learning_rate
+class HiddenLayer(Layer):
 
-total_error = 1
-while total_error > 0.001:
-    for j in range(10000):
-        a = random.randint(0, 1)
-        b = random.randint(0, 1)
-        input_vector = np.array([[a], [b]])
-        output_v = forward_prop(input_vector)
-        error_k = output_v - np.array([[a and b]])
-        error_value = np.sum(error_k**2)
-        backwards_prop(error_k, learning_rate=total_error/10)
+    def forward_prop(self, input_v):
+        self.prev_inputs.append(input_v)
+        a = self.weights.dot(input_v)
+        b = self.sigmoid(a)
+        return b
 
+class Network:
+    def __init__(self, layer_sizes):
+        self.hidden_layer = HiddenLayer(layer_sizes[0], layer_sizes[1])
+        self.output_layer = OutputLayer(layer_sizes[1], layer_sizes[2])
+
+    def forward_prop(self, input_v):
+        out = self.hidden_layer.forward_prop(input_v)
+        return self.output_layer.forward_prop(out)
+
+    def backwards_prop(self, error_k, learning_rate):
+        error = [error_k]
+        error = self.output_layer.backwards_prop(error, learning_rate)
+        error = self.hidden_layer.backwards_prop(error, learning_rate)
+        return error
+
+    def reset(self):
+        self.hidden_layer.reset()
+        self.output_layer.reset()
+
+def print_network_progress(net, iter_num):
     total_error = 0
+    print("Iter: {}".format(iter_num))
     for j in range(4):
         a = j & 1
         b = (j >> 1) & 1
         input_vector = np.array([[a], [b]])
-        output_v = forward_prop(input_vector)
+        output_v = net.forward_prop(input_vector)
         local_error = np.abs(output_v - np.array([[a and b]]))[0][0]
         total_error += local_error
         print("{} && {} = {:.6f} \tError: {:.6f}".format(a, b, output_v[0][0], local_error))
     print("Total Error: {:.6f}".format(total_error))
     print("---------")
+    net.reset()
+    return total_error
+
+total_error = 1
+iter_num = 1
+net = Network([2, 10, 1])
+while total_error > 0.01:
+    for j in range(10000):
+        a = random.randint(0, 1)
+        b = random.randint(0, 1)
+        input_vector = np.array([[a], [b]])
+
+        output_v = net.forward_prop(input_vector)
+        error_k = output_v - np.array([[a and b]])
+        net.backwards_prop(error_k, learning_rate=total_error/10)
+
+        error_value = np.sum(error_k**2)
+
+    total_error = print_network_progress(net, iter_num)
+
+    iter_num += 1
