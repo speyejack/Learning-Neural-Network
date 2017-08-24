f3dd687447ff67d43ac1dcc38539c496afcb7dce
Jack <speyejack@gmail.com>
2017-08-23 23:59:57 -0700

    First attempt at RNN, however, back prop is off

--- python/rnn.py
+++ python/rnn.py
@@ -1,12 +1,12 @@
 import numpy as np
-import pdb
-from time import sleep
+
 
 class Layer:
     def __init__(self, input_size, output_size):
         self.input_size = input_size
         self.output_size = output_size
-        self.weights = {"input": np.random.randn(output_size, input_size), "hidden": np.random.randn(output_size, output_size)}
+        self.weights = {"input": np.random.randn(output_size, input_size),
+                        "hidden": np.random.randn(output_size, output_size)}
         self.reset()
 
     def forward_prop(self, input_v):
@@ -19,7 +19,7 @@ class Layer:
         pass
 
     def backwards_prop(self, output_errors, learning_rate):
-        errors = self._backwards_prop(output_errors, learning_rate, 0, [[]])
+        errors = self._backwards_prop(output_errors, learning_rate, 0, [np.zeros_like(output_errors[0])])
         self.reset()
         return errors[:-1]
 
@@ -40,6 +40,7 @@ class Layer:
         output_v = input_v * (1 - input_v)
         return output_v
 
+
 class OutputLayer(Layer):
     def _forward_prop(self, input_v):
         a = self.weights["input"].dot(input_v)
@@ -47,34 +48,46 @@ class OutputLayer(Layer):
 
     def _backwards_prop(self, output_errors, learning_rate, depth, error_is):
         if not self.data["inputs"]:
-            self.weights["input"] -= self.adjustments["input"] * (learning_rate / depth)
+            self.weights["input"] -= self.adjustments["input"] * \
+                (learning_rate / depth)
             return error_is
         error_k = output_errors.pop()
         input_t = self.data["inputs"].pop()
-        error_i = self.weights["input"].T.dot(error_k) * self.deriv_sigmoid(input_t)
+        error_i = self.weights["input"].T.dot(error_k)
         self.adjustments["input"] += error_k.dot(input_t.T)
-        return self._backwards_prop(output_errors, learning_rate, depth + 1, [error_i] + error_is)
+        return self._backwards_prop(output_errors, learning_rate,
+                                    depth + 1, [error_i] + error_is)
+
 
 class HiddenLayer(Layer):
-
     def _forward_prop(self, input_v):
-        a = self.weights["input"].dot(input_v) + self.weights["hidden"].dot(self.data["outputs"][-1])
+        a = self.weights["input"].dot(input_v) +\
+            self.weights["hidden"].dot(self.data["outputs"][-1])
         b = self.sigmoid(a)
         return b
 
     def _backwards_prop(self, output_errors, learning_rate, depth, error_is):
         if not self.data["inputs"]:
-            self.weights["input"] -= self.adjustments["input"] * (learning_rate / depth)
+            scalar = (learning_rate / depth)
+            self.weights["input"] -= self.adjustments["input"] * scalar
+            self.weights["hidden"] -= self.adjustments["hidden"] * scalar
+
             return error_is
-        error_k = output_errors.pop()
+        error_k = self.deriv_sigmoid(self.data["outputs"].pop()) * \
+            (output_errors.pop() + self.weights["hidden"].T.dot(error_is[-1]))
         input_t = self.data["inputs"].pop()
-        error_i = self.weights["input"].T.dot(error_k) * self.deriv_sigmoid(input_t)
+        error_i = self.weights["input"].T.dot(error_k) * \
+            self.deriv_sigmoid(input_t)
         self.adjustments["input"] += error_k.dot(input_t.T)
-        return self._backwards_prop(output_errors, learning_rate, depth + 1, [error_i] + error_is)
+        self.adjustments["hidden"] += error_k.dot(self.data["outputs"][-1].T)
+        return self._backwards_prop(output_errors, learning_rate,
+                                    depth + 1, [error_i] + error_is)
 
     def reset(self):
         super().reset()
+        self.adjustments["hidden"] = np.zeros_like(self.weights["hidden"])
         self.data["outputs"].append(np.zeros((self.output_size, 1)))
+
 
 class Network:
     def __init__(self, layer_sizes):
@@ -95,38 +108,43 @@ class Network:
         self.hidden_layer.reset()
         self.output_layer.reset()
 
+
 def print_network_progress(net, iter_num):
     total_error = 0
     print("Iter: {}".format(iter_num))
-    for j in range(4):
-        a = j & 1
-        b = (j >> 1) & 1
-        input_vector = np.array([[a], [b]])
+    state = 0
+    states = [0, 1, 0]
+    for j in range(len(states)):
+        a = states.pop()
+        input_vector = np.array([[a]])
         output_v = net.forward_prop(input_vector)
-        local_error = np.abs(output_v - np.array([[a and b]]))[0][0]
+        state ^= a
+        local_error = np.abs(output_v - np.array([[state]]))[0][0]
         total_error += local_error
-        print("{} && {} = {:.6f} \tError: {:.6f}".format(a, b, output_v[0][0], local_error))
-    print("Total Error: {:.6f}".format(total_error))
+        print("state ^= {:d} = {:.3f} ({}) \tError: {:.3f}"
+              .format(a, output_v[0][0], state, local_error))
+    print("Total Error: {:.3f}".format(total_error))
     print("---------")
     net.reset()
     return total_error
 
-np.random.seed(25)
+
 total_error = 1
 iter_num = 1
-net = Network([2, 10, 1])
+net = Network([1, 10, 1])
 while total_error > 0.01:
     for j in range(1000):
         errors = []
-        for i in range(10):
+        state = 0
+        for i in range(3):
             a = np.random.randint(0, 2)
-            b = np.random.randint(0, 2)
-            input_vector = np.array([[a], [b]])
+            input_vector = np.array([[a]])
 
             output_v = net.forward_prop(input_vector)
-            error_k = output_v - np.array([[a and b]])
+            state ^= a
+            error_k = output_v - np.array([[state]])
             errors.append(error_k)
-        net.backwards_prop(errors, learning_rate=min(total_error, 0.9))
+        net.backwards_prop(errors, learning_rate=min(total_error, 0.0001))
 
         error_value = np.sum(error_k**2)
 
